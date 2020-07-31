0a8aabfc477061eeed9f0464c0db41106a60ac73
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-31 15:39:46 +0300

    change task 14

--- tasks/js/task-14.js
+++ tasks/js/task-14.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   'Вы согласны?',
+//   () => alert('Вы согласились.'),
+//   () => alert('Вы отменили выполнение.'),
+// );
+
+// let fruit = prompt('Какой фрукт купить?', 'apple');
+
+// let bag = {
+//   [fruit]: 5,
+// };
+
+// console.log(bag[fruit]);
+
+// let fruit = prompt('Какой фрукт купить?', 'apple');
+
+// let bag = {};
+// bag[fruit] = 10;
+
+// console.log(bag);
+// console.log(bag[fruit]);
+
+let fruit = 'apple';
+let bag = {
+  [fruit + ' toster']: 23,
+};
+
+console.log(bag);
