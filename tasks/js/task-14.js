894e8fe68533fcf75c7130159443ff4a8f61d051
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-31 17:36:35 +0300

    end work with task14

--- tasks/js/task-14.js
+++ tasks/js/task-14.js
@@ -27,9 +27,47 @@
 // console.log(bag);
 // console.log(bag[fruit]);
 
-let fruit = 'apple';
-let bag = {
-  [fruit + ' toster']: 23,
-};
+// let fruit = 'apple';
+// let bag = {
+//   [fruit + ' toster']: 23,
+// };
 
-console.log(bag);
+// console.log(bag);
+
+// let user = { noSuchProperty: 5 };
+
+// alert(user.noSuchProperty === undefined);
+
+// let user = {
+//   name: 'John',
+//   age: 39,
+//   face: 'scary',
+// };
+
+// console.log('agee' in user);
+
+let user = { name: 'John', age: 23 };
+
+let userClone = {};
+
+for (let key in user) {
+  userClone[key] = user[key];
+}
+
+console.log(userClone === user);
+console.log(userClone);
+
+userClone.name = 'Vadym';
+console.log(userClone);
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true, name: 'Dima' };
+
+Object.assign(userClone, permissions1, permissions2);
+
+console.log(userClone);
+
+let twoUserClone = Object.assign({}, userClone);
+
+console.log(twoUserClone.age++);
+console.log(twoUserClone, 'twoUserClone');
