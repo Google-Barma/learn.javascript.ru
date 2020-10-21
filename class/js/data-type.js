bcbb6b68639c907ddec5da9c3693b6c4cd633506
Vadym <vad.evlanov@gmail.com>
2020-10-21 13:56:04 +0300

    add rest

--- class/js/data-type.js
+++ class/js/data-type.js
@@ -10,23 +10,58 @@ const doublePrices = Object.fromEntries(
 
 console.log(doublePrices);
 
+// let salaries = {
+//   John: 100,
+//   Pete: 300,
+//   Mary: 250,
+// };
+
+// function sumSalaries(salariesArray) {
+//   return Object.keys(salariesArray).length > 0
+//     ? Object.values(salariesArray).reduce((acc, value) => acc + value)
+//     : 0;
+// }
+
+// console.log(sumSalaries(salaries));
+
+function count(obj) {
+  return Object.entries(obj).length;
+}
+
+let [firstName, lastName] = 'Vadym Yevlanov'.split(' ');
+
+console.log(firstName);
+
+let [a, b, c] = 'abc';
+
+console.log(a);
+
+let userData = { name: 'John', years: 30 };
+
+let { name, years, isAdmin = false } = userData;
+
+console.log(name);
+console.log(isAdmin);
+
 let salaries = {
   John: 100,
   Pete: 300,
   Mary: 250,
+  Vadym: 3500,
 };
 
-function sumSalaries(salariesArray) {
-  return Object.keys(salariesArray).length > 0
-    ? Object.values(salariesArray).reduce((acc, value) => acc + value)
-    : 0;
+function topSalaries(salaries) {
+  let maxSalary = 0;
+  let maxName = null;
+
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName;
 }
 
-console.log(sumSalaries(salaries));
-
-let user = {
-  name: 'John',
-  age: 30,
-};
-
-alert(count(user));
+console.log(topSalaries(salaries));
